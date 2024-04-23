c807789ece9c704f54c9b830cdac1894d34d7f1b
Kyamran Rizayev <kyamranr@gmail.com>
2024-04-22 20:52:38 -0500

    added class method

--- models.py
+++ models.py
@@ -1,6 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 db = SQLAlchemy()
+
+bcrypt = Bcrypt()
 
 def connect_db(app):
     """Connect to database"""
@@ -18,3 +21,12 @@ class User(db.Model):
     email = db.Column(db.String(50), nullable=False, unique=True)
     first_name = db.Column(db.String(30), nullable=False)
     last_name = db.Column(db.String(30), nullable=False)
+
+    @classmethod
+    def register(cls, username, password, email, first_name, last_name):
+        """Register user with hashed password"""
+        hashed = bcrypt.generate_password_hash(password)
+        hashed_utf8 = hashed.decode('utf8')
+        new_user = cls(username=username, password=hashed_utf8, email=email, first_name=first_name, last_name=last_name )
+        db.session.add(new_user)
+        return new_user
