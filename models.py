89236f8015e8e76478c3750d788c3e0bb33ecc3d
Kyamran Rizayev <kyamranr@gmail.com>
2024-04-23 20:04:40 -0500

    forgot to add unique to username

--- models.py
+++ models.py
@@ -16,7 +16,7 @@ class User(db.Model):
 
     __tablename__ = 'users'
 
-    username = db.Column(db.String(20), primary_key=True)
+    username = db.Column(db.String(20), primary_key=True, unique=True)
     password = db.Column(db.Text, nullable=False)
     email = db.Column(db.String(50), nullable=False, unique=True)
     first_name = db.Column(db.String(30), nullable=False)
