5e78d12e4329fc444763631803a9364fd1e71d3c
Kyamran Rizayev <kyamranr@gmail.com>
2024-04-23 21:20:54 -0500

    add all neccessary routes and templates and upated form.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, session, flash, url_for
-from models import db, connect_db, User
-from form import RegistrationFrom, LoginForm
+from models import db, connect_db, User, Feedback
+from form import RegistrationFrom, LoginForm, FeedbackForm
 
 
 app = Flask(__name__)
@@ -71,17 +71,88 @@ def logout_user():
 
 @app.route('/users/<username>')
 def logged_user(username):
-    """Logged in user information"""
+    """Logged in user information and show feedback"""
     if 'username' not in session or session['username'] != username:
+        flash('You are not authorized to view this page, please login.', 'error')
         return redirect('/login')
     
     user = User.query.filter_by(username=username).first()
-    if user:
-        return render_template('secret.html', user=user)
-    else:
+    if not user:
         return 'User not found.', 404
+        
+    feedbacks = Feedback.query.filter_by(username=username).all()
+    return render_template('secret.html', user=user, feedbacks=feedbacks)
+        
+@app.route('/users/<username>/delete', methods=['POST'])
+def delete_user(username):
+    """Delete user and associated feedback"""
+    if 'username' not in session or session['username'] != username:
+        flash('You are not authorized to delete this user.', 'error')
+        return redirect('/')
+    
+    user = User.query.filter_by(username=username).first()
+    if not user:
+        flash('User not found.', 'error')
+        return redirect('/')
+    
+    db.session.delete(user)
+    db.session.commit()
+    session.pop('username')
+    flash('User and associated feedback deleted successfully.', 'success')
+    return redirect('/')
+
+@app.route('/users/<username>/feedback/add', methods=['GET', 'POST'])
+def add_feedback(username):
+    """Add new feedback"""
+
+    if 'username' not in session or session['username'] != username:
+        flash('You are not authorized to add feedback for this user.', 'error')
+        return redirect('/')
+
+    form = FeedbackForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        content = form.content.data
+
+        feedback = Feedback(title=title, content=content, username=username)
+        db.session.add(feedback)
+        db.session.commit()
+        flash('Feedback added successfully.', 'success')
+        return redirect(url_for('logged_user', username=username))
+    return render_template('add_feedback.html', form=form)
+
+@app.route('/feedback/<int:feedback_id>/update', methods=['GET', 'POST'])
+def update_feedback(feedback_id):
+    """Update feedback"""
+    feedback = Feedback.query.get_or_404(feedback_id)
+    if 'username' not in session or session['username'] != feedback.username:
+        flash('You are not authorized to update this feedback.', 'error')
+        return redirect('/')
+    
+    form = FeedbackForm(obj=feedback)
+    if form.validate_on_submit():
+        feedback.title = form.title.data
+        feedback.content = form.content.data
+        
+        db.session.commit()
+        flash('Feedback updated successfully.', 'success')
+        return redirect(url_for('user_profile', username=feedback.username))
+    
+    return render_template('update_feedback.html', form=form)
 
 
+@app.route('/feedback/<int:feedback_id>/delete', methods=['POST'])
+def delete_feedback(feedback_id):
+    """Delete feedback"""
+    feedback = Feedback.query.get_or_404(feedback_id)
+    if 'username' not in session or session['username'] != feedback.username:
+        flash('You are not authorized to delete this feedback.', 'error')
+        return redirect('/')
+    
+    db.session.delete(feedback)
+    db.session.commit()
+    flash('Feedback deleted successfully.', 'success')
+    return redirect(url_for('user_profile', username=feedback.username))
 
 
 
--- form.py
+++ form.py
@@ -1,15 +1,25 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField, TextAreaField
 from wtforms.validators import InputRequired, Length
 
 class RegistrationFrom(FlaskForm):
-    username = StringField('Username:', validators=[InputRequired(), Length(max=20)])
-    password = PasswordField('Password:', validators=[InputRequired()])
+    """User registration form"""
+
+    username = StringField('Username:', validators=[InputRequired(), Length(min=1, max=20)])
+    password = PasswordField('Password:', validators=[InputRequired(), Length(min=6, max=50)])
     email = StringField('Email:', validators=[InputRequired(), Length(max=50)])
     first_name = StringField('First Name:', validators=[InputRequired(), Length(max=30)])
     last_name = StringField('Last Name:', validators=[InputRequired(), Length(max=30)])
 
 
 class LoginForm(FlaskForm):
+    """User login form"""
+
     username = StringField('Username:', validators=[InputRequired()])
     password = PasswordField('Password:', validators=[InputRequired()])
+
+class FeedbackForm(FlaskForm):
+    """User feedback form"""
+    
+    title = StringField('Title', validators=[InputRequired()])
+    content = TextAreaField('Content', validators=[InputRequired()])
